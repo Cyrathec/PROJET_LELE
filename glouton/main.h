dd37c234e1894e3850c5e46ff1333ed472074d72
Cyrathec <loic.did@gmail.com>
2020-11-19 09:34:25 +0100

    Premier commit - Lecture du fichier

--- glouton/main.h
+++ glouton/main.h
@@ -1,3 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <fstream>
+#include "utils.h"
+
+void getCost(int *cost, std::ifstream *instance, int n);
+void getCibles(int **cibles, std::ifstream *instance, int m);
+
 #endif // MAIN_H
